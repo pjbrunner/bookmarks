5164da542b140c1031f1b0f9b7b3f42145c568fa
pjbrunner <pjbrunner542@gmail.com>
2020-10-25 15:51:35 -0600

    Create Card class, rework how cards are created

--- cards.js
+++ cards.js
@@ -14,38 +14,55 @@ const links = {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelector('.cards');
+    const cardsDiv = document.querySelector('.cards');
 
     class Bookmarks {
-        constructor(cards) {
-            this.cards = cards;
-        };
+        constructor(cardsDiv) {
+            this.cards = [];
+            this.cardsDiv = cardsDiv;
+        }
 
         createCards() {
             for(let key in links) {
-                this.createCard(key);
+                let card = new Card(key, links[key].url, links[key].description, links[key].tags);
+                this.cardsDiv.appendChild(card.getCardDiv())
+                this.cards.push(card);
             }
-        }
-
-        createCard(name) {
-                let card = document.createElement('div');
-                card.classList.add('card');
-                let titleBox = document.createElement('div')
-                titleBox.classList.add('title-box');
-                let title = document.createElement('h3');
-                title.classList.add('gradient-title');
-                title.innerText = name;
-                let cardDesc = document.createElement('div');
-                cardDesc.classList.add('card-desc');
-                cardDesc.innerText = links[name].description;
-
-                titleBox.appendChild(title);
-                card.appendChild(titleBox);
-                card.appendChild(cardDesc);
-                this.cards.appendChild(card);
         }
     }
 
-    const bookmarks = new Bookmarks(cards);
+    class Card {
+        constructor(title, url, description, tags) {
+            this.title = title;
+            this.url = url;
+            this.description = description;
+            this.tags = tags;
+
+            this.cardDiv = document.createElement('div');
+            this.cardDiv.classList.add('card');
+            this.titleBoxDiv = document.createElement('div')
+            this.titleBoxDiv.classList.add('title-box');
+            this.titleDiv = document.createElement('h3');
+            this.titleDiv.classList.add('gradient-title');
+            this.titleDiv.innerText = this.title;
+            this.cardDescDiv = document.createElement('div');
+            this.cardDescDiv.classList.add('card-desc');
+            this.cardDescDiv.innerText = this.description;
+
+            this.constructCard();
+        }
+
+        getCardDiv() {
+            return this.cardDiv;
+        }
+
+        constructCard() {
+            this.titleBoxDiv.appendChild(this.titleDiv);
+            this.cardDiv.appendChild(this.titleBoxDiv);
+            this.cardDiv.appendChild(this.cardDescDiv);
+        }
+    }
+
+    const bookmarks = new Bookmarks(cardsDiv);
     bookmarks.createCards();
 });
